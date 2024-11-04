2f2f5a50150546521f84c5fce6deed944740925c
limboden <imbodenlu@gmail.com>
2024-11-04 18:38:45 -0500

    created candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface CandidateInterface {
+  name: string,
+  username: string,
+  location: string,
+  avatar: string,
+  email: string,
+  html_url: string,
+  company: string,
+}
+//done?
