6210626ca37c3763f54d0f73f687481af5847051
limboden <imbodenlu@gmail.com>
2024-11-30 17:55:52 -0500

    updated nav

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -3,11 +3,25 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/SavedCandidates">Saved Candidates</Link></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse " id="navbarNav">
+          <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <h5>
+                <Link className="nav-link text-light" to="/">
+                  Home
+                </Link>
+              </h5>
+              <h5>
+                <Link className="nav-link text-light" to="/SavedCandidates">
+                  Potential Candidates
+                </Link>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 };
--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,4 +1,3 @@
-// TODO: Create an interface for the Candidate objects returned by the API
 export default interface Candidate {
   login: string;
   id: number;
