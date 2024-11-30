350cab1a70f9c0e6e5ad5df317b87941efafde38
limboden <imbodenlu@gmail.com>
2024-11-30 14:30:13 -0500

    added a use effect and moved some things around, because it was formatted weird

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
-
-const searchGitHubUsers = async (query: string) => {
-  const response = await fetch(`https://api.github.com/search/users?q=${query}`, {
-    headers: {
-      Authorization: `token ${process.env.VITE_GITHUB_TOKEN}`
-    }
-  });
-
-  if (!response.ok) {
-    throw new Error('response was not ok');
-  }
-
-  const data = await response.json();
-  return data.items; // returns a list of users
-};
-
-const handleSearch = async () => {
-  try {
-    const users = await searchGitHubUsers('octocat');
-    console.log(users); // this is where we do stuff with the search results
-    localStorage.setItem('searchedUsers', JSON.stringify(users));
-  } catch (error) {
-    console.error('Error fetching users:', error);
-  }
-};
-
 
 const CandidateSearch = () => {
 
   const [users, setUsers] = useState([]);
+
+  const searchGitHubUsers = async (query: string) => {
+    const response = await fetch(`https://api.github.com/search/users?q=${query}`, {
+      headers: {
+        Authorization: `token ${process.env.VITE_GITHUB_TOKEN}`
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('response was not ok');
+    }
+
+    const data = await response.json();
+    return data.items; // returns a list of users
+  };
+
+  const handleSearch = async () => {
+    try {
+      const users = await searchGitHubUsers('octocat');
+      console.log(users); // this is where we do stuff with the search results
+      localStorage.setItem('searchedUsers', JSON.stringify(users));
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    const saved = localStorage.getItem('savedCandidates');
+    if (saved) {
+      //retrieve saved candidates
+    }
+  }, []);
 
 
 
