27d8741ff3fb56c63240550cc3e1413aa316fa38
limboden <imbodenlu@gmail.com>
2024-11-30 18:17:16 -0500

    started the Saved

--- Develop/src/pages/SavedCandidates.tsx
+++ Develop/src/pages/SavedCandidates.tsx
@@ -1,4 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Candidate from '../interfaces/Candidate.interface';
+
+
+
+
+
 const SavedCandidates = () => {
+
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem('savedCandidates');
+    if (saved) {
+      setSavedCandidates(JSON.parse(saved));
+    }
+  }, []);
+
+
   return (
     <>
       <h1>Potential Candidates</h1>
