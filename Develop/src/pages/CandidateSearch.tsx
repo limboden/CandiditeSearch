ea88b03a303448b7467bc26dce741c3d48428915
limboden <imbodenlu@gmail.com>
2024-11-30 10:57:37 -0500

    i guess i forgot to add some things to git. oops

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,7 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
+const searchGitHubUsers = async (query: string) => {
+  const response = await fetch(`https://api.github.com/search/users?q=${query}`, {
+    headers: {
+      Authorization: `token ${process.env.VITE_GITHUB_TOKEN}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error('response was not ok');
+  }
+
+  const data = await response.json();
+  return data.items; // returns a list of users
+};
+
+const handleSearch = async () => {
+  try {
+    const users = await searchGitHubUsers('octocat');
+    console.log(users); // this is where we do stuff with the search results
+    localStorage.setItem('searchedUsers', JSON.stringify(users));
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+};
+
+
 const CandidateSearch = () => {
+
+  const [users, setUsers] = useState([]);
+
+
+
+
   return (
     <div>
       <h1>CandidateSearch</h1>
