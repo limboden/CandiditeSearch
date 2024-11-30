c8c288d8b31a2faf22e601906aac840f4784cf19
limboden <imbodenlu@gmail.com>
2024-11-30 17:53:34 -0500

    return html (:

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -4,7 +4,6 @@ import Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
 
-  const [users, setUsers] = useState([]);
   const [candidates, setCandidates] = useState<Candidate[]>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [currentCandidate, setCurrentCandidate] = useState<Candidate | null>(null)
@@ -46,7 +45,7 @@ const CandidateSearch = () => {
 
   //what happens when someone saves a candidate
   const handleSave = () => {
-    const newSavedCandidatesArray = [...savedCandidates, currentCandidate]
+    const newSavedCandidatesArray = [...savedCandidates, currentCandidate as Candidate]
     setsavedCandidates(newSavedCandidatesArray)
     localStorage.setItem('savedCandidates', JSON.stringify(savedCandidates))
     handleNextArrayItem();
@@ -64,8 +63,26 @@ const CandidateSearch = () => {
 
 
   return (
-    <div>
-      <h1>CandidateSearch</h1>
+    <div className="candidate-search-container container-fluid">
+      <h1 className='page-titles'>Candidate Search</h1>
+      {currentCandidate && (
+        <div className="card p-3 mb-2 bg-dark text-white">
+          <div>
+            <img src={currentCandidate.avatar_url} alt={currentCandidate.login} className="card-img-top img-fluid" />
+          </div>
+          <div className="card-body">
+            <h2>{currentCandidate.name || currentCandidate.login} <span>({currentCandidate.login})</span></h2>
+            <p className="card-text">Location: {currentCandidate.location || 'N/A'}</p>
+            <p className="card-text">Email: <a href={`mailto:${currentCandidate.email}`}>{currentCandidate.email || 'N/A'}</a></p>
+            <p className="card-text">Company: {currentCandidate.company || 'N/A'}</p>
+            <p className="card-text">Profile: <a href={currentCandidate.html_url} target="_blank" rel="noopener noreferrer">{currentCandidate.html_url}</a></p>
+          </div>
+          <div className="candidate-actions button-group">
+            <button type="button" className="btn btn-danger" onClick={() => handleNextArrayItem()}>Deny</button>
+            <button type="button" className="btn btn-success" onClick={() => handleSave()}>Accept</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
