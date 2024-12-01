6e6769ec5f198f8dcfbdbbe216b50690b3a15ee7
limboden <imbodenlu@gmail.com>
2024-11-30 21:31:29 -0500

    wrote handleRejectCandidate, and the html for the saved candidate component

--- Develop/src/pages/SavedCandidates.tsx
+++ Develop/src/pages/SavedCandidates.tsx
@@ -16,11 +16,59 @@ const SavedCandidates = () => {
     }
   }, []);
 
+  const handleRejectCandidate = (id: number) => {
+    const newCandidates = savedCandidates.filter((candidate) => candidate.id !== id); // put all the ones that aren't the one we removed into the new candidates
+    setSavedCandidates(newCandidates);
+    localStorage.setItem('savedCandidates', JSON.stringify(newCandidates));
+  };
+
+
 
   return (
-    <>
+    <div className="saved-candidates-container container">
       <h1>Potential Candidates</h1>
-    </>
+      {/* Conditional rendering for "No candidates" message */}
+      {savedCandidates.length === 0 ? (
+        <p className="text-center mt-4">No candidates have been accepted yet!</p>
+      ) : (
+        <table className="candidate-table table-bordered table table-dark table-striped table-hover">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Bio</th>
+              <th>Reject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {savedCandidates.map((candidate) => (
+              <tr key={candidate.id}>
+                <td>
+                  <img src={candidate.avatar_url} alt={candidate.login} width={50} height={50} />
+                </td>
+                <td>
+                  {candidate.name || candidate.login} <span>({candidate.login})</span>
+                </td>
+                <td>{candidate.location || 'N/A'}</td>
+                <td>
+                  <a href={`mailto:${candidate.email}`}>{candidate.email || 'N/A'}</a>
+                </td>
+                <td>{candidate.company || 'N/A'}</td>
+                <td>{candidate.bio || 'N/A'}</td>
+                <td>
+                  <div className='button-group'>
+                    <button type="button" className="btn btn-danger" onClick={() => handleRejectCandidate(candidate.id)}>-</button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
